a74dfe220ff2cd8df02eb032a27fc3e1eec8323b
bhuvanrajmp <105975325+bhuvanrajmp@users.noreply.github.com>
2026-07-08 16:23:30 +0530

    Sync: Two Sum (47 ms, 46.9 MB)

--- 0001-two-sum/solution.java
+++ 0001-two-sum/solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> seen = new HashMap<>();
+    
         for(int i=0;i<=nums.length;i++){
-            int secondEle = target - nums[i];
-            if(seen.containsKey(secondEle)){
-                return new int[] { seen.get(secondEle), i };
-            }
-            seen.put(nums[i], i);
+            for(int j=i+1;j<=nums.length-1;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[] { i, j };
+                }
+            
         }
-        return new int[0];
         }
+         return new int[0]; // no solution found
+    }
 }
