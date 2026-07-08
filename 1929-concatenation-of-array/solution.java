d7fe48bb085ef83e3a46d7e0e4de8201b89227be
bhuvanrajmp <105975325+bhuvanrajmp@users.noreply.github.com>
2026-07-08 15:39:54 +0530

    Sync: Concatenation of Array (1 ms, 47.2 MB)

---
+++ 1929-concatenation-of-array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+        int size =  nums.length;
+        int[] ans= new int[2 *size];
+      
+        for (int i = 0 ; i<size; i++)
+        {
+            ans[i]=nums[i];
+            ans[i+size]=nums[i];
+            
+        }
+        return ans;
+
+    }
+}
